eda6199475a385cbb503e9831a93b07501fa7d98
Pradip Das <pradipdev446@gmail.com>
2026-08-01 07:53:03 -0700

    Add C++ Solution to Patterns/Strings

--- LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.cpp
+++ LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        int i = 0;
+        int n = s.length();
         
+        // 1. Skip leading whitespaces
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+        
+        // 2. Determine sign
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+        
+        // 3. Convert digits to integer and handle overflow
+        int result = 0;
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+            
+            // Check for overflow/underflow before updating result
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)) {
+                return sign == 1 ? INT_MAX : INT_MIN;
+            }
+            
+            result = result * 10 + digit;
+            i++;
+        }
+        
+        return result * sign;
     }
 };
